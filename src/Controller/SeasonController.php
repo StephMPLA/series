a065388ce349c1afc2d264208ff60ca151ee7767
StephMPLA <stephane.mougeot@hotmail.fr>
2024-09-10 15:37:45 +0200

    Module 7 doctrine FIN

---
+++ src/Controller/SeasonController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Season;
+use App\Form\SeasonType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SeasonController extends AbstractController
+{
+    #[Route('/season/create', name: 'season_create')]
+    public function create(Request $request,EntityManagerInterface $entityManager): Response
+    {
+        //Crée une nouvelle saison
+        $season = new Season();
+        //Formulaire
+        $seasonForm = $this->createForm(SeasonType::class, $season);
+
+        $seasonForm->handleRequest($request);
+
+        if($seasonForm->isSubmitted() && $seasonForm->isValid()){
+            $entityManager->persist($season);
+            $entityManager->flush();
+
+
+        }
+
+
+        return $this->render('season/create.html.twig', [
+            'seasonForm' => $seasonForm->createView(),
+        ]);
+    }
+}
